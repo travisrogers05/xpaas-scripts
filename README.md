1e4a7280fb1dbb1031d5e1522bc39ca83f129f05
Travis Rogers <trogers@redhat.com>
2019-07-29 18:49:27 -0500

    Updating script and readme

--- README.md
+++ README.md
@@ -6,8 +6,32 @@ Utility scripts for Red Hat Openshift middleware containers
 Used to update the imagestreams and templates for Red Hat middleware container images.
 
 ### Configuration
-- `REPOURL` - defaults to the Github repository for the Openshift application templates
-- `TAG` - used to determine the version of the imagestreams and templates to get
+- `REPOURL` - defaults to the Github repository for the Red Hat JBoss middleware images
+- `RHEAP6_REPOURL` - defaults to the Github repository for the JBoss EAP 6.4.x Openshift image
+- `RHEAP6_BRANCH` - branch used to clone the imagestreams and templates for JBoss EAP 6.4.x
+- `RHDM_REPOURL` - defaults to the Github repository for the Red Hat Decision Manager Openshift image
+- `RHDM_BRANCH` - branch used to clone the imagestreams and templates for Red Hat Decision Manager
+- `RHPAM_REPOURL` - defaults to the Github repository for the Red Hat Process Automation Manager Openshift image
+- `RHPAM_BRANCH` - branch used to clone the imagestreams and templates for Red Hat Process Automation Manager
+- `RHSSO_REPOURL` - defaults to the Github repository for the Red Hat Single Sign On Openshift image
+- `RHSSO_BRANCH` - branch used to clone the imagestreams and templates for Red Hat Single Sign On
+- `RHJWS3_REPOURL` - defaults to the Github repository for the Red Hat JBoss Webserver 3 Openshift image
+- `RHJWS3_BRANCH` - branch used to clone the imagestreams and templates for Red Hat JBoss Webserver 3
+- `RHJWS5_REPOURL` - defaults to the Github repository for the Red Hat JBoss Webserver 5 Openshift image
+- `RHJWS5_BRANCH` - branch used to clone the imagestreams and templates for Red Hat JBoss Webserver 5
+- `RHAMQ_REPOURL` - defaults to the Github repository for the Red Hat AMQ Openshift image
+- `RHAMQ_BRANCH` - branch used to clone the imagestreams and templates for Red Hat AMQ
+- `RHDG_REPOURL` - defaults to the Github repository for the Red Hat Datagrid Openshift image
+- `RHDG_BRANCH` - branch used to clone the imagestreams and templates for Red Hat Datagrid
+- `RHDV_REPOURL` - defaults to the Github repository for the Red Hat Datavirt Openshift image
+- `RHDV_BRANCH` - branch used to clone the imagestreams and templates for Red Hat Datavirt
+- `RHEAP_REPOURL` - defaults to the Github repository for the Red Hat JBoss EAP 7 Openshift image
+- `RHEAP_BRANCH` - branch used to clone the imagestreams and templates for Red Hat JBoss EAP 7
+- `RHPS_REPOURL` - defaults to the Github repository for the Red Hat Process Server 6 Openshift image
+- `RHPS_BRANCH` - branch used to clone the imagestreams and templates for Red Hat Process Server 6
+- `RHDS_REPOURL` - defaults to the Github repository for the Red Hat Decision Server 6 Openshift image
+- `RHDS_BRANCH` - branch used to clone the imagestreams and templates for Red Hat Decision Server 6
+
 
 ### Running
 This script should be executed as an Openshift user that has `system:admin` or cluster admin privileges.  The script uses the Openshift command line client and git. 
--- update-xpaas.sh
+++ update-xpaas.sh
@@ -7,61 +7,137 @@
 # Must have OCP admin rights to perform these tasks
 #-------------------------------------------------------------------
 
-# The repo url
-REPOURL=https://github.com/jboss-openshift/application-templates
+CWD=`pwd`
 
-# Set the desired tag to use
-TAG=ose-v1.4.12
+# JBoss EAP 6.4.x
+RHEAP6_REPOURL=https://github.com/jboss-openshift/application-templates
+RHEAP6_BRANCH=master
 
-# get a specific tag of the jboss-openshift repo
-git clone $REPOURL
-cd application-templates
-git checkout $TAG
+# Newer locations for imagestreams and templates
+REPOURL=https://github.com/jboss-container-images
+RHDM_REPOURL=$REPOURL/rhdm-7-openshift-image
+RHDM_BRANCH=7.4.x
+RHPAM_REPOURL=$REPOURL/rhpam-7-openshift-image
+RHPAM_BRANCH=7.4.x
+RHSSO_REPOURL=$REPOURL/redhat-sso-7-openshift-image
+RHSSO_BRANCH=sso72-dev
+RHJWS3_REPOURL=$REPOURL/jboss-webserver-3-openshift-image
+RHJWS3_BRANCH=webserver31-dev
+RHJWS5_REPOURL=$REPOURL/jboss-webserver-5-openshift-image
+RHJWS5_BRANCH=webserver50-dev
+RHAMQ_REPOURL=$REPOURL/jboss-amq-7-broker-openshift-image
+RHAMQ_BRANCH=amq71-dev
+RHDG_REPOURL=$REPOURL/jboss-datagrid-7-openshift-image
+RHDG_BRANCH=datagrid73-dev
+RHDV_REPOURL=$REPOURL/jboss-datavirt-6-openshift-image
+RHDV_BRANCH=datavirt64-dev
+RHEAP_REPOURL=$REPOURL/jboss-eap-7-openshift-image
+RHEAP_BRANCH=eap72-dev
+RHPS_REPOURL=$REPOURL/jboss-processserver-6-openshift-image
+RHPS_BRANCH=6.4.x
+RHDS_REPOURL=$REPOURL/jboss-decisionserver-6-openshift-image
+RHDS_BRANCH=6.4.x
 
-ISLIST=`find . -name "*.json" | grep -v '/secrets/' | grep -v 'image-streams' | grep 'image-stream'`
-for stream in $ISLIST ;
- do
-   # delete current imagestream
-   cat ${stream} | oc -n openshift delete -f -
-   # create new imagestream
-   oc -n openshift create -f ${stream}
- done
+function updateImagestreams () {
+  TARGET_DIR=$1
+  
+  # change to a specific child directory
+  cd $TARGET_DIR
 
+  ISLIST=`ls *.@(json|yaml) | grep -v '/secrets/' | grep 'image-stream'`
+  COUNT=`wc -l $ISLIST`
+  if [ "$COUNT" -eq "0" ]; then
+    echo "Unable to find any imagestreams in $TARGET_DIR"
+    cd $CWD
+    return
+  fi
 
-# delete any existing xpaas templates
-TEMPLATELIST=`find . -name "*.json" | grep -v '/secrets/' | grep -v 'image-stream' | cut -c 3- | cut -d '/' -f2 | rev | cut -c 6- | rev`
-for template in $TEMPLATELIST ;
- do
-   oc -n openshift delete template ${template}
- done
+  # update the imagestreams
+  for stream in $ISLIST ;
+  do
+    echo "updating imagestream ${stream}"
+    # delete current imagestream
+#    cat ${stream} | oc -n openshift delete -f -
+    # create new imagestream
+#    oc -n openshift create -f ${stream}
+  done
 
-# load the new xpaas templates
-TEMPLATELIST=`find . -name "*.json" | grep -v '/secrets/' | grep -v 'image-stream'`
-for template in $TEMPLATELIST ;
- do
-   oc -n openshift create -f ${template}
- done
+  # reset the current directory to the parent directory
+  cd $CWD
+}
 
+function updateTemplates () {
+  TARGET_DIR=$1
+  
+  # change to a specific child directory
+  cd $TARGET_DIR
 
-# ensure the image versions are available (fixes a bug)
-#for is in jboss-eap71-openshift \
-#  jboss-eap64-openshift \
-#  jboss-webserver31-tomcat7-openshift \
-#  jboss-webserver31-tomcat8-openshift \
-#  jboss-datagrid71-openshift \
-#  jboss-datagrid71-client-openshift \
-#  jboss-datavirt64-openshift \
-#  jboss-datavirt64-driver-openshift \
-#  jboss-processserver64-openshift \
-#  jboss-decisionserver64-openshift \
-#  jboss-amq-63 \
-#  redhat-openjdk18-openshift \
-#  redhat-sso72-openshift ;
-#do
-#  oc -n openshift import-image ${is} --all=true
-#done
+  # get the list of template file names
+  TEMPLATELIST=`ls *.@(json|yaml) | grep -v '/secrets/' | grep -v 'image-stream'`
+  COUNT=`wc -l $TEMPLATELIST`
+  if [ "$COUNT" -eq "0" ]; then
+    echo "Unable to find any templates in $TARGET_DIR"
+    cd $CWD
+    return
+  fi
 
+  # delete any existing templates
+  for template in $TEMPLATELIST ;
+  do
+    echo "deleting template ${template}"
+#    cat ${template} | oc -n openshift delete template -f -
+  done
 
- 
+  # create templates
+  for template in $TEMPLATELIST ;
+  do
+    echo "creating template ${template}"
+#    oc -n openshift create -f ${template}
+  done
+
+  # reset the current directory to the parent directory
+  cd $CWD
+}
+
+# clone specific branches from the repos
+git clone -b $RHEAP6_BRANCH $RHEAP6_REPOURL
+git clone -b $RHDM_BRANCH $RHDM_REPOURL
+git clone -b $RHPAM_BRANCH $RHPAM_REPOURL
+git clone -b $RHSSO_BRANCH $RHSSO_REPOURL
+git clone -b $RHJWS3_BRANCH $RHJWS3_REPOURL
+git clone -b $RHJWS5_BRANCH $RHJWS5_REPOURL
+git clone -b $RHAMQ_BRANCH $RHAMQ_REPOURL
+git clone -b $RHDG_BRANCH $RHDG_REPOURL
+git clone -b $RHDV_BRANCH $RHDV_REPOURL
+git clone -b $RHEAP_BRANCH $RHEAP_REPOURL
+git clone -b $RHPS_BRANCH $RHPS_REPOURL
+git clone -b $RHDS_BRANCH $RHDS_REPOURL
+
+# update the imagestreams and templates
+updateImagestreams $(basename $RHEAP6_REPOURL)/eap
+updateTemplates $(basename $RHEAP6_REPOURL)/eap
+updateImagestreams $(basename $RHDV_REPOURL)/resources/openshift
+updateTemplates $(basename $RHDV_REPOURL)/resources/openshift
+updateImagestreams $(basename $RHDM_REPOURL)
+updateTemplates $(basename $RHDM_REPOURL)/templates
+updateImagestreams $(basename $RHPAM_REPOURL)
+updateTemplates $(basename $RHPAM_REPOURL)/templates
+updateImagestreams $(basename $RHSSO_REPOURL)/templates
+updateTemplates $(basename $RHSSO_REPOURL)/templates
+updateImagestreams $(basename $RHJWS3_REPOURL)/templates
+updateTemplates $(basename $RHJWS3_REPOURL)/templates
+updateImagestreams $(basename $RHJWS5_REPOURL)/templates
+updateTemplates $(basename $RHJWS5_REPOURL)/templates
+updateImagestreams $(basename $RHAMQ_REPOURL)/templates
+updateTemplates $(basename $RHAMQ_REPOURL)/templates
+updateImagestreams $(basename $RHDG_REPOURL)/templates
+updateTemplates $(basename $RHDG_REPOURL)/templates
+updateImagestreams $(basename $RHEAP_REPOURL)/templates
+updateTemplates $(basename $RHEAP_REPOURL)/templates
+updateImagestreams $(basename $RHPS_REPOURL)/templates
+updateTemplates $(basename $RHPS_REPOURL)/templates
+updateImagestreams $(basename $RHDS_REPOURL)/templates
+updateTemplates $(basename $RHDS_REPOURL)/templates
+
 
 
